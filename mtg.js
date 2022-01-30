b903217be480d5177c2d29e2495a7450cacc87d5
unknown <anik.bmsit@gmail.com>
2022-01-30 15:35:52 +0530

    First Add

---
+++ mtg.js
@@ -0,0 +1,18 @@
+var a = prompt("Enter your full name");
+var a1 = a;
+var b= prompt("Enter your contact number");
+var c = prompt("Enter your location");
+var c1 = c;
+var d= prompt("Enter your address");
+var e = prompt("Enter your preferred timing(AM/PM)");
+var e1 = e;
+
+document.getElementById("fn").value = a;
+document.getElementById("c").value = b;
+document.getElementById("lo").value = c;
+document.getElementById("add").value = d;
+document.getElementById("time").value = e;
+
+document.getElementById("fn1").value = a1;
+document.getElementById("lo1").value = c1;
+document.getElementById("time1").value = e1;
